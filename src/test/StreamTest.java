6cd4233586e5ce49c8da0dd7040f7b4e9adb22aa
hwqing <hwqing@outlook.com>
2024-03-27 16:02:09 +0800

    流测试

--- src/test/StreamTest.java
+++ src/test/StreamTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @Title: StreamTest
@@ -16,5 +17,13 @@ public class StreamTest {
         List<String> testList = new ArrayList<>(Arrays.asList("2", "5", "8"));
         Set<String> testSet = new HashSet<>(Arrays.asList("1", "7"));
         System.out.println(testList.stream().anyMatch(testSet::contains));
+
+        List mapList = new ArrayList();
+        for (int i = 0; i < 10; i++) {
+            Map map = new HashMap();
+            map.put("object_id", i + "_aaa");
+            mapList.add(map);
+        }
+        System.out.println(mapList.stream().map(map->((Map)map).get("object_id").toString().split("_")[0]).collect(Collectors.toList()));
     }
 }

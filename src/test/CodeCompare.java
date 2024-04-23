a54a54c7df2bf8b48430df186db91cda5afd02dc
hwqing <hwqing@outlook.com>
2024-04-23 16:26:54 +0800

    字符比较

---
+++ src/test/CodeCompare.java
@@ -0,0 +1,17 @@
+package test;
+
+
+/**
+ * @Title: CodeCompare
+ * @Description:
+ * @Create time: 2024/4/23 15:05
+ * @Author: hwqing
+ * @Version 1.0
+ */
+public class CodeCompare {
+    public static void main(String[] args) {
+        String a = "A";
+        String b = "B";
+        System.out.println(a.charAt(0) < b.charAt(0));
+    }
+}
